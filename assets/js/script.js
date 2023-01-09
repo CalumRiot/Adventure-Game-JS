de399d4dffee1746c1ed3d8f73ea559625795306
CalumRiot <calumgriffin@yahoo.com>
2023-01-09 12:34:11 +0000

    list game functions in JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,16 @@
 const textElement = document.getElementById('text')
 const optionButtonsElement = document.getElementById('option-buttons')
 
+function startGame() {
+
+}
+
+function showTextNode(textNodeIndex) {
+
+}
+
+function selectOption(option) {
+
+}
+
+startGame()
